22790426103c3fc312cae82fda3df7c683cfb683
andrewpoock <66223719+andrewpoock@users.noreply.github.com>
2020-06-24 14:20:53 -0500

    Add files via upload

--- sort_search.py
+++ sort_search.py
@@ -1,17 +1,19 @@
 def list():
-    x = [65, 18, 22, 14, 34]
+    x = [2, 4, 1, 3]
     print(search_list(x))
-    print(sort_list(x))
+    sort_list(x)
 
 
 def sort_list(x):
-    return x.sort() #included return so I can print the sorted list from the main list function
+    x.sort()
+    print(x)
+    return x #included return so I can print the sorted list from the main list function
 
 
 def search_list(x):
     try:
         index = x.index(14)
-        return 'The index of 14:', index
+        return ('The index of 14 is ' + str(index))
     except:
         return -1
 
--- test_sort_search.py
+++ test_sort_search.py
@@ -1,17 +1,17 @@
 from fun_with_collections import sort_search
 import unittest
-import unittest.mock as mock
+
 
 class TestListElements(unittest.TestCase):
     def test_sort(self):
-        with mock.patch('builtins.input', side_effect=[2, 1, 3]):
-            assert sort_search.sort_list() == [1, 2, 3]
+        expected_list = sort_search.sort_list([2, 4, 1, 3])
+        self.assertEqual(expected_list, [1, 2, 3, 4])
+
     def test_found(self):
-        with mock.patch('builtins.input', side_effect=[2, 14, 3]):
-            assert sort_search.search_list() == 'The index of 14: 1'
-    def test_found(self):
-        with mock.patch('builtins.input', side_effect=[2, 1, 3]):
-            assert sort_search.search_list() == -1
+        assert sort_search.search_list([2, 14, 3]) == 'The index of 14 is 1'
+
+    def test_not_found(self):
+        assert sort_search.search_list([2, 1, 3]) == -1
 
 
 if __name__ == '__main__':
